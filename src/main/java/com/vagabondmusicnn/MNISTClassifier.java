7238b15886ec2ae4aa02a698fbf0f082971595ed
Bungee54 <evanlyuba@gmail.com>
2016-11-13 20:11:46 -0500

    Downloaded documentation and sources, updated MNISTClassifier.java

--- src/main/java/com/vagabondmusicnn/MNISTClassifier.java
+++ src/main/java/com/vagabondmusicnn/MNISTClassifier.java
@@ -1,6 +1,12 @@
 package com.vagabondmusicnn;
 
+import org.bytedeco.javacv.ImageTransformer;
 import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.nd4j.linalg.dataset.api.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 
 import java.io.IOException;
@@ -19,9 +25,18 @@ public class MNISTClassifier {
 
         try {
             DataSetIterator mnistTrain = new MnistDataSetIterator(batchSize, true, rngSeed);
+            DataSetIterator mnistTest = new MnistDataSetIterator(batchSize, false, rngSeed);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(rngSeed)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .iterations(1)
+                .learningRate(0.006)
+                .updater(Updater.NESTEROVS).momentum(0.9)
+                .regularization(true).l2(1e-4)
+                .list().build();
     }
 }
