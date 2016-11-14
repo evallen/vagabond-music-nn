28bea2e68c17ec230580ac6c3e172d59dd2e6178
Bungee54 <evanlyuba@gmail.com>
2016-11-13 19:46:03 -0500

    Started working on a test classifier for MNIST handwritten data.

---
+++ src/main/java/com/vagabondmusicnn/MNISTClassifier.java
@@ -0,0 +1,14 @@
+package com.vagabondmusicnn;
+
+/**
+ * Created by Evan on 11/13/2016.
+ */
+public class MNISTClassifier {
+    public static void main (String[] args) {
+        final int numRows = 28;
+        final int numColumns = 28;
+        int outputNum = 10;
+        int batchSize = 128;
+        int rngSeed = 123;
+    }
+}
