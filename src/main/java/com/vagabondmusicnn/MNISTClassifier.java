4feebec3fd62819d5c52c847ad7e1a4d57c7a276
Bungee54 <evanlyuba@gmail.com>
2016-11-13 19:51:19 -0500

    Started working on a test classifier for MNIST handwritten data.

--- src/main/java/com/vagabondmusicnn/MNISTClassifier.java
+++ src/main/java/com/vagabondmusicnn/MNISTClassifier.java
@@ -1,5 +1,10 @@
 package com.vagabondmusicnn;
 
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+
+import java.io.IOException;
+
 /**
  * Created by Evan on 11/13/2016.
  */
@@ -10,5 +15,13 @@ public class MNISTClassifier {
         int outputNum = 10;
         int batchSize = 128;
         int rngSeed = 123;
+        int numEpochs = 15;
+
+        try {
+            DataSetIterator mnistTrain = new MnistDataSetIterator(batchSize, true, rngSeed);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
