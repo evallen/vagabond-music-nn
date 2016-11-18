b545285795f9dbdbd1b75000e2711560cea27076
Bungee54 <evanlyuba@gmail.com>
2016-11-17 19:09:34 -0500

    tried to fix log4j but didnt lol

--- src/main/java/com/vagabondmusicnn/MNISTClassifier.java
+++ src/main/java/com/vagabondmusicnn/MNISTClassifier.java
@@ -1,13 +1,22 @@
 package com.vagabondmusicnn;
 
-import org.bytedeco.javacv.ImageTransformer;
 import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -15,7 +24,10 @@ import java.io.IOException;
  * Created by Evan on 11/13/2016.
  */
 public class MNISTClassifier {
-    public static void main (String[] args) {
+
+    private static Logger log = LoggerFactory.getLogger(MNISTClassifier.class);
+
+    public static void main (String[] args) throws IOException {
         final int numRows = 28;
         final int numColumns = 28;
         int outputNum = 10;
@@ -23,13 +35,12 @@ public class MNISTClassifier {
         int rngSeed = 123;
         int numEpochs = 15;
 
-        try {
-            DataSetIterator mnistTrain = new MnistDataSetIterator(batchSize, true, rngSeed);
-            DataSetIterator mnistTest = new MnistDataSetIterator(batchSize, false, rngSeed);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        DataSetIterator mnistTrain = new MnistDataSetIterator(batchSize, true, rngSeed);
+        DataSetIterator mnistTest = new MnistDataSetIterator(batchSize, false, rngSeed);
 
+
+
+        log.info("Build model...");
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .seed(rngSeed)
                 .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
@@ -37,6 +48,45 @@ public class MNISTClassifier {
                 .learningRate(0.006)
                 .updater(Updater.NESTEROVS).momentum(0.9)
                 .regularization(true).l2(1e-4)
-                .list().build();
+                .list()
+
+                .layer(0, new DenseLayer.Builder()
+                    .nIn(numRows * numColumns)
+                    .nOut(1000)
+                    .activation("relu")
+                    .weightInit(WeightInit.XAVIER)
+                    .build())
+
+                .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                      .nIn(1000)
+                    .nOut(outputNum)
+                    .activation("softmax")
+                    .weightInit(WeightInit.XAVIER)
+                    .build())
+
+                .pretrain(false).backprop(true)
+                .build();
+
+        MultiLayerNetwork model = new MultiLayerNetwork (conf);
+        model.init();
+
+        model.setListeners(new ScoreIterationListener(1));
+
+        log.info("Train model...");
+        for (int i = 0; i < numEpochs; i++) {
+            model.fit(mnistTrain);
+        }
+
+        log.info("Evaluate model ...");
+        Evaluation eval = new Evaluation(outputNum);
+        while (mnistTest.hasNext()) {
+            DataSet next = mnistTest.next();
+            INDArray output = model.output(next.getFeatureMatrix());
+            eval.eval(next.getLabels(), output);
+        }
+
+        log.info(eval.stats());
+        log.info("***********Example finished**********");
+
     }
 }
