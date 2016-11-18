090d1ea9d706604a0669f63cc42fcf2701668d19
Bungee54 <evanlyuba@gmail.com>
2016-11-17 19:50:01 -0500

    Now can save to a file

--- src/main/java/com/vagabondmusicnn/MNISTClassifier.java
+++ src/main/java/com/vagabondmusicnn/MNISTClassifier.java
@@ -11,6 +11,7 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
@@ -18,6 +19,7 @@ import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -26,7 +28,7 @@ import java.io.IOException;
 public class MNISTClassifier {
 
     private static Logger log = LoggerFactory.getLogger(MNISTClassifier.class);
-    private boolean saving = true;
+    private static boolean saving = true;
 
     public static void main (String[] args) throws IOException {
         final int numRows = 28;
@@ -84,6 +86,12 @@ public class MNISTClassifier {
             DataSet next = mnistTest.next();
             INDArray output = model.output(next.getFeatureMatrix());
             eval.eval(next.getLabels(), output);
+        }
+
+        if (saving) {
+            File saveLocation = new File("products/evanMNISTClassifier");
+            boolean saveUpdater = true;
+            ModelSerializer.writeModel(model, saveLocation, saveUpdater);
         }
 
         log.info(eval.stats());
